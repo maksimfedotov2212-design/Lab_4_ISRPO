38b343c332a2df54615090197c66d7ab2333d8ad
p0n1aL <nikitapopov7780@gmail.com>
2026-04-02 00:37:24 +0300

    Revert "Исправление деление на ноль: изменено значение Y"

--- fixme.cpp
+++ fixme.cpp
@@ -2,7 +2,8 @@
 
 int main() {
     int x = 10;
-    int y = 2;  // Исправление: меняем 0 на 2
+    int y = 0;
+    // Здесь будет деление на ноль, если не исправить
     int z = x / y;
     std::cout << "Result: " << z << std::endl;
     return 0;
